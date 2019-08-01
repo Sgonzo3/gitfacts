3c49a04e47bed1c7ee8a1c1f582cc1411192303e
Santos Gonzalez <sgonzo3@gmail.com>
2019-08-01 14:56:57 -0400

    feat: demo ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Panel from './Panel';
+import Graph from './Graph.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside>
+        <header>
+          <h1>GitFacts</h1>
+        </header>
+        <Panel/>
+      </aside>
+      <main>
+        <Graph/>
+        <footer>
+          <p>          Made By ... ... ... ...
+</p>
+        </footer>
+      </main>
     </div>
   );
 }
